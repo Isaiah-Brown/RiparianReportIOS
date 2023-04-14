5a6c6ddddd307103728e9f36432b388d40d8f4da
Isaiah-Brown <ibrown@fandm.edu>
2023-04-14 11:08:16 -0400

    Initial Commit

---
+++ RiparianReport/RiparianReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiparianReportApp.swift
+//  RiparianReport
+//
+//  Created by TLC Loaner on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct RiparianReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
